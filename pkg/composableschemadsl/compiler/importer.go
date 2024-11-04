bc5980ee86d35423483b0c38148b904ce2a4fbc5
Tanner Stirrat <tstirrat@gmail.com>
2024-11-04 12:44:39 -0700

    Add importer logic

---
+++ pkg/composableschemadsl/compiler/importer.go
@@ -0,0 +1,39 @@
+package compiler
+
+import (
+	"fmt"
+	"path"
+	"os"
+
+	"github.com/authzed/spicedb/pkg/composableschemadsl/input"
+	"github.com/authzed/spicedb/pkg/genutil/mapz"
+	"github.com/rs/zerolog/log"
+)
+
+const SchemaFileSuffix = ".zed"
+
+func ImportFile(pathSegments []string, existingNames *mapz.Set[string]) (*CompiledSchema, error) {
+	filepath := constructFilePath(pathSegments)
+
+	var schemaBytes []byte
+	schemaBytes, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read schema file: %w", err)
+	}
+	// TODO: do we want this sort of logging here?
+	log.Trace().Str("schema", string(schemaBytes)).Str("file", filepath).Msg("read schema from file")
+
+	compiled, err := Compile(InputSchema{
+		// TODO: should this point to the schema file? What is this for?
+		Source:       input.Source("schema"),
+		SchemaString: string(schemaBytes),
+	}, AllowUnprefixedObjectType(), ExistingNames(existingNames.AsSlice()))
+	if err != nil {
+		return nil, err
+	}
+	return compiled, nil
+}
+
+func constructFilePath(segments []string) string {
+	return path.Join(segments...) + SchemaFileSuffix
+}
